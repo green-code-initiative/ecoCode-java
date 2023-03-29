152cd8900b2aebcbb596bce5407d54b72050b7d4
David DE CARVALHO <ddecarvalho@solocal.com>
2023-03-29 23:44:01 +0200

    correction of compile pbs to send to sonar next

--- src/main/java/fr/greencodeinitiative/java/checks/AvoidSetConstantInBatchUpdateCheck.java
+++ src/main/java/fr/greencodeinitiative/java/checks/AvoidSetConstantInBatchUpdateCheck.java
@@ -1,21 +1,22 @@
 package fr.greencodeinitiative.java.checks;
 
 import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.PreparedStatement;
-import java.util.regex.Pattern;
+import java.sql.SQLException;
 import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 class AvoidSetConstantInBatchUpdateCheck {
 
-    void literalSQLrequest() { //dirty call
+    void literalSQLrequest() throws SQLException { //dirty call
 
         int x = 0;
         Connection con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "system", "oracle");
         PreparedStatement stmt = con.prepareStatement("insert into Emp values(?,?,?,?)");
         stmt.setInt(1, 101);
         stmt.setString(2, "Ratan");
-        stmt.setBigDecimal(3, Bigdecimal.ONE);
+        stmt.setBigDecimal(3, BigDecimal.ONE);
         stmt.setBigDecimal(4, BigDecimal.valueOf(x));
         stmt.setBoolean(5, Boolean.valueOf("true"));
         int i = stmt.executeUpdate();
@@ -23,7 +24,7 @@ class AvoidSetConstantInBatchUpdateCheck {
         con.close();
     }
 
-    void batchInsertInForLoop(int[] data) {
+    void batchInsertInForLoop(int[] data) throws SQLException {
 
         Connection con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "system", "oracle");
         PreparedStatement stmt = con.prepareStatement("insert into Emp values(?,?,?,?,?,?,?,?,?,?,?)");
@@ -32,7 +33,7 @@ class AvoidSetConstantInBatchUpdateCheck {
 
             stmt.setBoolean(2, true); // Noncompliant {{Avoid setting constants in batch update}}
             stmt.setByte(3, (byte) 3); // Noncompliant {{Avoid setting constants in batch update}}
-            stmt.setByte(4, 'v'); // Noncompliant {{Avoid setting constants in batch update}}
+            stmt.setByte(4, (byte) 'v'); // Noncompliant {{Avoid setting constants in batch update}}
             stmt.setShort(5, (short) 5); // Noncompliant {{Avoid setting constants in batch update}}
             stmt.setInt(6, 6); // Noncompliant {{Avoid setting constants in batch update}}
             stmt.setLong(7, (long) 7); // Noncompliant {{Avoid setting constants in batch update}}
@@ -46,12 +47,12 @@ class AvoidSetConstantInBatchUpdateCheck {
             stmt.addBatch();
         }
         int[] nr = stmt.executeBatch();
-        logger.log("{} rows updated", IntStream.of(nr).sum());
+        System.out.printf("{} rows updated", IntStream.of(nr).sum());
         con.close();
     }
 
 
-    int[] batchInsertInForeachLoop(DummyClass[] data) {
+    int[] batchInsertInForeachLoop(DummyClass[] data) throws SQLException {
 
         try (Connection con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "system", "oracle")) {
             PreparedStatement stmt = con.prepareStatement("insert into Emp values(?,?,?,?,?,?,?,?,?,?,?,?,?)");
@@ -59,7 +60,7 @@ class AvoidSetConstantInBatchUpdateCheck {
                 stmt.setInt(1, o.getField1());
                 stmt.setBoolean(2, Boolean.valueOf("false")); // Noncompliant {{Avoid setting constants in batch update}}
                 stmt.setByte(3, o.getField3());
-                stmt.setByte(4, 'v'); // Noncompliant {{Avoid setting constants in batch update}}
+                stmt.setByte(4, (byte) 'v'); // Noncompliant {{Avoid setting constants in batch update}}
                 stmt.setShort(5, (short) 5); // Noncompliant {{Avoid setting constants in batch update}}
                 stmt.setInt(6, 6); // Noncompliant {{Avoid setting constants in batch update}}
                 stmt.setLong(7, 7); // Noncompliant {{Avoid setting constants in batch update}}
@@ -74,7 +75,7 @@ class AvoidSetConstantInBatchUpdateCheck {
     }
 
 
-    int[] batchInsertInWhileLoop(DummyClass[] data) {
+    int[] batchInsertInWhileLoop2(DummyClass[] data) throws SQLException {
 
         try (Connection con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "system", "oracle")) {
             PreparedStatement stmt = con.prepareStatement("insert into Emp values(?,?,?,?,?,?,?,?,?,?,?,?,?)");
@@ -85,7 +86,7 @@ class AvoidSetConstantInBatchUpdateCheck {
                 stmt.setBoolean(2, Boolean.TRUE); // Noncompliant {{Avoid setting constants in batch update}}
                 stmt.setByte(3, o.getField3());
                 stmt.setByte(4, Byte.MAX_VALUE); // Noncompliant {{Avoid setting constants in batch update}}
-                stmt.setByte(4, Character.MAX_VALUE); // Noncompliant {{Avoid setting constants in batch update}}
+                stmt.setByte(4, (byte) Character.MAX_VALUE); // Noncompliant {{Avoid setting constants in batch update}}
                 stmt.setShort(5, Short.MIN_VALUE); // Noncompliant {{Avoid setting constants in batch update}}
                 stmt.setInt(6, Integer.MAX_VALUE); // Noncompliant {{Avoid setting constants in batch update}}
                 stmt.setLong(7, Long.MIN_VALUE); // Noncompliant {{Avoid setting constants in batch update}}
@@ -100,7 +101,7 @@ class AvoidSetConstantInBatchUpdateCheck {
         }
     }
 
-    int[] batchInsertInWhileLoop(DummyClass[] data) {
+    int[] batchInsertInWhileLoop(DummyClass[] data) throws SQLException {
         if (data.length == 0) {
             return new int[]{};
         }
@@ -113,11 +114,10 @@ class AvoidSetConstantInBatchUpdateCheck {
                 stmt.setBoolean(2, Boolean.valueOf(true)); // Noncompliant {{Avoid setting constants in batch update}}
                 stmt.setByte(3, o.getField3());
                 stmt.setByte(4, Byte.valueOf((byte) 3)); // Noncompliant {{Avoid setting constants in batch update}}
-                stmt.setByte(4, Character.valueOf('1')); // Noncompliant {{Avoid setting constants in batch update}}
                 stmt.setShort(5, Short.valueOf((short) 55)); // Noncompliant {{Avoid setting constants in batch update}}
                 stmt.setInt(6, Integer.valueOf("222")); // Noncompliant {{Avoid setting constants in batch update}}
                 stmt.setLong(7, Long.valueOf(0)); // Noncompliant {{Avoid setting constants in batch update}}
-                stmt.setFloat(8, Float.valueOf(.33)); // Noncompliant {{Avoid setting constants in batch update}}
+                stmt.setFloat(8, Float.valueOf(.33f)); // Noncompliant {{Avoid setting constants in batch update}}
                 stmt.setDouble(9, Double.valueOf(22));  // Noncompliant {{Avoid setting constants in batch update}}
                 stmt.setString(10, o.getField2());
                 stmt.setBigDecimal(11, BigDecimal.valueOf(11)); // Noncompliant {{Avoid setting constants in batch update}}
@@ -143,8 +143,9 @@ class AvoidSetConstantInBatchUpdateCheck {
         }
 
         public double getField4() {
-            return .1; }
-	}
-	
+            return .1;
+        }
+    }
+
 
 }
