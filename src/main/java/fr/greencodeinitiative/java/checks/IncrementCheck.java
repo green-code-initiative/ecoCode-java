48aacb3b255fe58731a4443d753e41a490e8f060
David DE CARVALHO <dedece35@gmail.com>
2023-04-10 10:45:58 +0200

    [ISSUE 60] adding test use cases

--- src/main/java/fr/greencodeinitiative/java/checks/IncrementCheck.java
+++ src/main/java/fr/greencodeinitiative/java/checks/IncrementCheck.java
@@ -30,7 +30,19 @@ class IncrementCheck {
     }
 
     void foo4(int value) {
-        int counter =0;
+        int counter = 0;
         counter = counter + 35 + 78 ;
+    }
+
+    void foo50(int value) {
+        for (int i=0; i < 10; i++) { // Noncompliant {{Use ++i instead of i++}}
+            System.out.println(i);
+        }
+    }
+
+    void foo51(int value) {
+        for (int i=0; i < 10; ++i) {
+            System.out.println(i);
+        }
     }
 }
