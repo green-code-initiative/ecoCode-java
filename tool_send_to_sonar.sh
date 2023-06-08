78003eca15b35a970701cf68464686e774450516
David DE CARVALHO <dedece35@gmail.com>
2023-06-08 08:13:40 +0200

    upgrade authent method for SonarQube

--- tool_send_to_sonar.sh
+++ tool_send_to_sonar.sh
@@ -2,4 +2,4 @@
 
 # "sonar.login" variable : private TOKEN generated in your local SonarQube during installation
 # (input paramater of this script)
-mvn org.sonarsource.scanner.maven:sonar-maven-plugin:3.9.1.2184:sonar -Dsonar.login=$1
+mvn org.sonarsource.scanner.maven:sonar-maven-plugin:3.9.1.2184:sonar -Dsonar.token=$1
