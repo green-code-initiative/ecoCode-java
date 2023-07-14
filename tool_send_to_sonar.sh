6cf57753a9d53fe98b21aa1201ee1103366745ca
David DE CARVALHO <dedece35@gmail.com>
2023-07-14 22:03:26 +0700

    update sonar scanner command

--- tool_send_to_sonar.sh
+++ tool_send_to_sonar.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env sh
 
-# "sonar.login" variable : private TOKEN generated in your local SonarQube during installation
+# "sonar.token" variable (or sonar.login before SONARQUBE 9.9) : private TOKEN generated in your local SonarQube during installation
 # (input paramater of this script)
 mvn org.sonarsource.scanner.maven:sonar-maven-plugin:3.9.1.2184:sonar -Dsonar.token=$1
+
+# command if you have a SONARQUBE < 9.9 (sonar.token existing for SONARQUBE >= 10.0)
+# mvn org.sonarsource.scanner.maven:sonar-maven-plugin:3.9.1.2184:sonar -Dsonar.login=$1
