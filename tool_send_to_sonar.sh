5ce922a0c2a7cc27ecce75f8276676ed9771c83d
David DE CARVALHO <dedece35@gmail.com>
2024-01-17 08:15:35 +0100

    [TECH] add comment

--- tool_send_to_sonar.sh
+++ tool_send_to_sonar.sh
@@ -3,6 +3,7 @@
 # "sonar.token" variable (or sonar.login before SONARQUBE 9.9) : private TOKEN generated in your local SonarQube during installation
 # (input paramater of this script)
 mvn org.sonarsource.scanner.maven:sonar-maven-plugin:3.9.1.2184:sonar -Dsonar.token=$1
+# mvn org.sonarsource.scanner.maven:sonar-maven-plugin:3.9.1.2184:sonar -Dsonar.token=$1 -Dsonar.host.url=https://sonar-staging.gcp.cicd.solocal.com/
 
 # command if you have a SONARQUBE < 9.9 (sonar.token existing for SONARQUBE >= 10.0)
 # mvn org.sonarsource.scanner.maven:sonar-maven-plugin:3.9.1.2184:sonar -Dsonar.login=$1
