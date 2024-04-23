1b337366534a778aa3f01d4fbe6f91da80afcf0d
David DE CARVALHO <dedece35@gmail.com>
2024-04-23 23:06:24 +0200

    Add where clause managment

--- src/main/java/fr/greencodeinitiative/java/checks/OptimizeSQLQueriesWithLimit.java
+++ src/main/java/fr/greencodeinitiative/java/checks/OptimizeSQLQueriesWithLimit.java
@@ -1,3 +1,20 @@
+/*
+ * ecoCode - Java language - Provides rules to reduce the environmental footprint of your Java programs
+ * Copyright © 2023 Green Code Initiative (https://www.ecocode.io)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 package fr.greencodeinitiative.java.checks;
 
 import org.sonar.check.Rule;
@@ -15,13 +32,15 @@ import static java.util.regex.Pattern.compile;
 @Rule(key = "EC24")
 public class OptimizeSQLQueriesWithLimit extends IssuableSubscriptionVisitor {
 
-    public static final String MESSAGE_RULE = "Optimize Database SQL Queries (Clause LIMIT)";
+    public static final String MESSAGE_RULE = "Optimize Database SQL Queries (Clause LIMIT / WHERE)";
     private static final Predicate<String> LIMIT_REGEXP =
             compile("limit", CASE_INSENSITIVE).asPredicate();
     private static final Predicate<String> SELECT_REGEXP =
             compile("select", CASE_INSENSITIVE).asPredicate();
     private static final Predicate<String> FROM_REGEXP =
             compile("from", CASE_INSENSITIVE).asPredicate();
+    private static final Predicate<String> WHERE_REGEXP =
+            compile("where", CASE_INSENSITIVE).asPredicate();
 
     @Override
     public List<Tree.Kind> nodesToVisit() {
@@ -31,8 +50,12 @@ public class OptimizeSQLQueriesWithLimit extends IssuableSubscriptionVisitor {
     @Override
     public void visitNode(Tree tree) {
         String value = ((LiteralTree) tree).value();
-        if (SELECT_REGEXP.test(value) && FROM_REGEXP.test(value) && !LIMIT_REGEXP.test(value)) {
-            reportIssue(tree, MESSAGE_RULE);
+        if (SELECT_REGEXP.test(value)
+            && FROM_REGEXP.test(value)
+            && !LIMIT_REGEXP.test(value)
+            && !WHERE_REGEXP.test(value)
+            ) {
+                reportIssue(tree, MESSAGE_RULE);
         }
     }
 
--- src/test/files/OptimizeSQLQueriesWithLimit.java
+++ src/test/files/OptimizeSQLQueriesWithLimit.java
@@ -1,16 +1,36 @@
+/*
+ * ecoCode - Java language - Provides rules to reduce the environmental footprint of your Java programs
+ * Copyright © 2023 Green Code Initiative (https://www.ecocode.io)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 class OptimizeSQLQueriesWithLimit {
 
     public void literalSQLrequest() {
-        dummyCall("SELECT user FROM myTable"); // Noncompliant {{Optimize Database SQL Queries (Clause LIMIT)}}
+        dummyCall("SELECT user FROM myTable"); // Noncompliant {{Optimize Database SQL Queries (Clause LIMIT / WHERE)}}
         dummyCall("SELECT user FROM myTable LIMIT 50"); // Compliant
+        dummyCall("SELECT user FROM myTable WHERE user.name = 'titi'"); // Compliant
     }
 
-    @Query("select t from Todo t where t.status != 'COMPLETED'") // Noncompliant {{Optimize Database SQL Queries (Clause LIMIT)}}
+    @Query("select t from Todo t") // Noncompliant {{Optimize Database SQL Queries (Clause LIMIT / WHERE)}}
+    @Query("select t from Todo t where t.status != 'COMPLETED'") // Compliant
     @Query("select t from Todo t where t.status != 'COMPLETED' LIMIT 25") // Compliant
 
     private void callQuery() {
-        String sql1 = "SELECT user FROM myTable"; // Noncompliant {{Optimize Database SQL Queries (Clause LIMIT)}}
+        String sql1 = "SELECT user FROM myTable"; // Noncompliant {{Optimize Database SQL Queries (Clause LIMIT / WHERE)}}
         String sql2 = "SELECT user FROM myTable LIMIT 50"; // Compliant
+        String sql3 = "SELECT user FROM myTable WHERE user.name = 'titi'"; // Compliant
     }
 
     private void dummyCall(String request) {
