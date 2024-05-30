2facdf253f689c780366ea2a60b031f3a757ee47
Denielig <denielig.frelicot@i-bp.fr>
2024-05-30 11:46:01 +0200

    Fix: fix class and file name

--- src/test/java/fr/greencodeinitiative/java/checks/UseEveryColumnQueriedTest.java
+++ src/test/java/fr/greencodeinitiative/java/checks/UseEveryColumnQueriedTest.java
@@ -20,7 +20,7 @@ package fr.greencodeinitiative.java.checks;
 import org.junit.jupiter.api.Test;
 import org.sonar.java.checks.verifier.CheckVerifier;
 
-class UserEveryColumnQueriedTest {
+class UseEveryColumnQueriedTest {
 
     @Test
     void testHasIssues() {
