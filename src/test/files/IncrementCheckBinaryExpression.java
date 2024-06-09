b59cd541a4a71b1658b06b85ef46c44542032b72
Hocine Hacherouf <hacherouf.hocine@gmail.com>
2024-06-09 20:09:45 +0200

    Update java classes files used in unit tests for EC67

--- src/test/files/IncrementCheckBinaryExpression.java
+++ src/test/files/IncrementCheckBinaryExpression.java
@@ -15,8 +15,8 @@
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
-class MyClass2 {
-    MyClass(MyClass mc) {
+class IncrementCheckBinaryExpression {
+    IncrementCheckBinaryExpression(IncrementCheckBinaryExpression mc) {
     }
     void unaryExpressionWithinBinaryExpression() {
         var i = 0;
