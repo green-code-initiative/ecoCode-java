c0251589700af491130d626dec6fbaa3abc66751
David DE CARVALHO <dedece35@gmail.com>
2024-07-19 16:34:54 +0200

    update docker port managment

--- tool_send_to_sonar.sh
+++ tool_send_to_sonar.sh
@@ -2,8 +2,7 @@
 
 # "sonar.token" variable (or sonar.login before SONARQUBE 9.9) : private TOKEN generated in your local SonarQube during installation
 # (input paramater of this script)
-mvn clean org.sonarsource.scanner.maven:sonar-maven-plugin:3.9.1.2184:sonar -Dsonar.token=$1
-# mvn org.sonarsource.scanner.maven:sonar-maven-plugin:3.9.1.2184:sonar -Dsonar.token=$1 -Dsonar.host.url=https://sonar-staging.gcp.cicd.solocal.com/
+mvn org.sonarsource.scanner.maven:sonar-maven-plugin:3.9.1.2184:sonar -Dsonar.host.url=http://localhost:$1 -Dsonar.token=$2
 
 # command if you have a SONARQUBE < 9.9 (sonar.token existing for SONARQUBE >= 10.0)
 # mvn org.sonarsource.scanner.maven:sonar-maven-plugin:3.9.1.2184:sonar -Dsonar.login=$1
