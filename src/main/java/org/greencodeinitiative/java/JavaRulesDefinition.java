6b58d4197802b565b767fa043626057277f82453
Vincent Marmin <3215889+vincent314@users.noreply.github.com>
2024-12-06 16:56:17 +0100

    chore: set Copyright year to 2024

--- src/main/java/org/greencodeinitiative/java/JavaRulesDefinition.java
+++ src/main/java/org/greencodeinitiative/java/JavaRulesDefinition.java
@@ -1,6 +1,6 @@
 /*
  * ecoCode - Java language - Provides rules to reduce the environmental footprint of your Java programs
- * Copyright © 2023 Green Code Initiative (https://www.ecocode.io)
+ * Copyright © 2024 Green Code Initiative (https://www.ecocode.io)
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
